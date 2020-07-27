e7f6a4fc0384222935ee89a5faac6f1efb75f8ad
Emmanuela <emmanuela.ezeakacha@xputertechnologies.com>
2020-07-27 13:27:32 +0100

    removed commented out code

--- server.ts
+++ server.ts
@@ -2,59 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// app.use('/js', express.static(__dirname + '/js'));
-// app.use('/dist', express.static(__dirname + '/../dist'));
-// app.use('/css', express.static(__dirname + '/css'));
-// app.use('/partials', express.static(__dirname + '/partials'));
-// app.use(express.static(__dirname + '/src/app'));
-// app.use(express.static(__dirname + '/src/app/app.component.html'));
-
-// app.all('/*', (req, res, next) => {
-//     // Just send the index.html for other files to support HTML5Mode
-//     res.sendFile('app.component.html', { root: __dirname + '/src/app/' });
-//     // res.sendFile(path.join(ROOTDIR, 'dist/My-Research-Portfolio/index.html'));
-// });
-
-// app.set('views', './app/views');
-
 app.use(express.static(__dirname +  '/dist/My-Research-Portfolio'));
 
-// app.use('/docs', express.static('./src/app/undergrad-docs/undergrad-docs.component.html'));
-
-
-// app.use('/docs', (req, res) => {
-//   // app.use(express.static(path.join(ROOTDIR, 'src/app/undergrad-docs/undergrad-docs.component.html')));
-//   res.sendFile(path.join(ROOTDIR, 'src/app/undergrad-docs/undergrad-docs.component.html'));
-//   res.render(path.join(ROOTDIR, 'src/app/undergrad-docs/undergrad-docs.component.html'));
-//   // res.sendFile(path.join(ROOTDIR, 'src/app/undergrad-docs/undergrad-docs.component.html'));
-// });
-// app.use('/publications', (req, res) => {
-//   app.use(express.static(path.join(ROOTDIR, 'src/app/publications/publications.component.html')));
-//   // res.sendFile(path.join(ROOTDIR, 'src/app/undergrad-docs/undergrad-docs.component.html'));
-// });
-// app.use('/resume', (req, res) => {
-//   app.use(express.static(path.join(ROOTDIR, 'src/app/curriculum-vitae/curriculum-vitae.component.html')));
-//   // res.sendFile(path.join(ROOTDIR, 'src/app/undergrad-docs/undergrad-docs.component.html'));
-// });
-
-
-// app.use('/publications', (req, res) => {
-//   res.sendFile(path.join(ROOTDIR, 'src/app/publications/publications.component.html'));
-// });
-// app.use('/resume', (req, res) => {
-//   res.sendFile(path.join(ROOTDIR, 'src/app/curriculum-vitae/curriculum-vitae.component.html'));
-// });
-
-
-// app.use(express.static(path.join(ROOTDIR, '/src/app/app.component.html')));
-// app.use(express.static('./src/index.html'));
-
 app.get('/*', (req, res) => {
-  // res.sendFile(path.join(__dirname + '/dist/my-research-portfolio/index.html'));
-  // res.sendFile(path.join('./dist/my-research-portfolio/index.html'));
   res.sendFile('index.html', { root: './dist/My-Research-Portfolio'});
-  // res.sendFile(path.join(ROOTDIR, 'src/app/mainpage/mainpage.component.html'));
-  // res.sendFile('app.component.html', { root: 'src/app/'});
 });
 
 // app.listen(process.env.PORT || 3000);
